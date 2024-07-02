9d6b9b238a8b1fe501f8e7a1635b8571e9983753
SRubtsov <frexrub@gmail.com>
2024-07-02 10:17:37 +0300

    add encode_jwt and decode_jwt(utils)

--- serts/utils.py
+++ serts/utils.py
@@ -1,5 +1,21 @@
 import jwt
 
-encoded = jwt.encode({"some": "payload"}, "secret", algorithm="HS256")
+from core.config import setting
 
-decodet = jwt.decode(encoded, "secret", algorithms=["HS256"])
+
+def encode_jwt(
+        payload: dict,
+        private_key: str = setting.auth_jwt.private_key_path.read_text(),
+        algorithm: str = setting.auth_jwt.algorithm,
+):
+    encoded = jwt.encode(payload, private_key, algorithm=algorithm)
+    return encoded
+
+
+def decode_jwt(
+        token: str | bytes,
+        public_key: str = setting.auth_jwt.public_key_path.read_text(),
+        algorithm: str = setting.auth_jwt.algorithm,
+):
+    decoded = jwt.decode(token, public_key, algorithms=[algorithm])
+    return decoded
