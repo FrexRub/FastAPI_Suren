95004bff91f919340d61b97085805e656affb14a
SRubtsov <frexrub@gmail.com>
2024-07-02 11:20:21 +0300

    edit demo_jwt_aut

--- api_v1/demo_auth/demo_jwt_aut.py
+++ api_v1/demo_auth/demo_jwt_aut.py
@@ -1,5 +1,17 @@
+from fastapi import APIRouter, Depends
+
+from pydantic import BaseModel
+
 from users.schemas import UserSchema
 from auth import utils as auth_utils
+
+
+class TokenInfo(BaseModel):
+    access_token: str
+    token_type: str
+
+
+router = APIRouter(prefix="/jwt", tags=["JWT"])
 
 john = UserSchema(
     username="john",
@@ -13,5 +25,26 @@ sam = UserSchema(
 )
 
 user_db: dict[str, UserSchema] = {
-
+    john.username: john,
+    sam.username: sam,
 }
+
+
+def validate_auth_user():
+    pass
+
+
+@router.post("/login", response_model=TokenInfo)
+def auth_user_issue_jwt(
+        user: UserSchema = Depends(validate_auth_user),
+):
+    jwt_payload = {
+        "sub": user.username,
+        "username": user.username,
+        "email": user.email,
+    }
+    token = auth_utils.encode_jwt(jwt_payload)
+    return TokenInfo(
+        access_token=token,
+        token_type="Bearer",
+    )
